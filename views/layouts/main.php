d66f5cef2ff4dc8553952b64e1963bebbb51ec5f
ampleeve <e@ampleev.com>
2017-02-23 12:50:13 +0300

    dsf

---
+++ views/layouts/main.php
@@ -0,0 +1,12 @@
+<?php /** @var \app\models\Product $model*/ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Site</title>
+</head>
+<body>
+<?= $content ?>
+</body>
+</html>
