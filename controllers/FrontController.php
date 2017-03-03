46591baac23250bf8aed4fcd58424fe81ad7d732
ampleeve <e@ampleev.com>
2017-03-03 09:13:57 +0300

    frontcontroller_alpha

---
+++ controllers/FrontController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evgenijampleev
+ * Date: 03.03.17
+ * Time: 0:14
+ */
+
+namespace app\controllers;
+
+
+class FrontController
+{
+
+}
---
+++ services/RequestManager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evgenijampleev
+ * Date: 03.03.17
+ * Time: 0:38
+ */
+
+namespace app\services;
+
+
+class RequestManager
+{
+
+}
