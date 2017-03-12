031432f1312a9f55ccfe23f5848142f9a2d682e1
ampleeve <e@ampleev.com>
2017-03-12 20:25:01 +0300

    sdfjn

--- base/Container.php
+++ base/Container.php
@@ -5,11 +5,27 @@
  * Date: 12.03.17
  * Time: 17:47
  */
-
 namespace app\base;
+class Container{
 
+    protected $items = [];
 
-class Container
-{
+    public function set($object, $key){
+
+        $this->items[$key] = $object;
+
+    }
+
+    public function get($key){
+
+        if(!isset($this->items[$key])){
+
+            $this->items[$key] = Application::call()->createComponent($key);
+
+        }
+
+        return $this->items[$key];
+
+    }
 
 }
